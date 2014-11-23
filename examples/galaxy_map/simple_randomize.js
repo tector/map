d07157c88596b1819f3235d0c14980462681fa4f
tector <cotoyomi@yahoo.de>
2014-11-23 21:22:47 +0100

    changed galaxy map example code

--- examples/galaxy_map/simple_randomize.js
+++ examples/galaxy_map/simple_randomize.js
@@ -3,8 +3,31 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var xmin = 1185;
+var xmax = 1254;
+var ymin = 1185;
+var ymax = 1254;
+
+var environments = [
+    'asteroids-1', 'asteroids-1-brown', 'asteroids-2', 'asteroids-2-brown',
+    'fog-blue', 'fog-red', 'fog-green', 'fog-grey',
+    'fog-magenta', 'fog-red', 'fog-yellow'];
+
+console.log('planets');
+
+// planets
 for (var i=1; i<=100; i++) {
-    x = getRandomInt(1185, 1254);
-    y = getRandomInt(1185, 1254);
+    x = getRandomInt(xmin, xmax);
+    y = getRandomInt(ymin, ymax);
     console.log('{"layer": 1, "x": '+x+', "y": '+y+', "attribs":{"title":"test '+x+'-'+y+'", "class":"sprites planet-'+i%10+'"}},');
+}
+
+console.log('environments');
+
+// environments
+for (var i=1; i<=200; i++) {
+    x = getRandomInt(xmin, xmax);
+    y = getRandomInt(ymin, ymax);
+    idx = Math.floor(Math.random() * 11);
+    console.log('{"layer": 0, "x": '+x+', "y": '+y+', "attribs":{"title":"test '+x+'-'+y+'", "class":"sprites '+environments[idx]+'"}},');
 }
